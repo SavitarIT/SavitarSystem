db9434b4f56b63d6fbed4408616bf74bc69b0bdd
Michael Smit <msmit@savitar.co.za>
2021-07-31 20:57:15 +0200

    Adding in an aspnet core backend for API calls

---
+++ Savitar.WebAssembly/EnvironmentInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Savitar.WebAssembly
+{
+    public class EnvironmentInfo
+    {
+        public static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
